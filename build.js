1730eec06178522e59f3a8d551a8a2231b4a3cc9
luobotang <tangkangxing@sina.com>
2018-09-03 16:18:19 +0800

    增加更多输出格式，准备 npm 发布

--- build.js
+++ build.js
@@ -2,10 +2,18 @@ const rollup = require('rollup')
 const commonjs = require('rollup-plugin-commonjs')
 
 const inputOpts = {input: './index.js', plugins: [commonjs()]}
-const outputOpts = {file: './dist/watch-it.js', format: 'iife', name: 'WatchIt', exports: 'named'}
+const outputIifeOpts = {file: './dist/watch-it.js', format: 'iife', name: 'WatchIt', exports: 'named'}
+const outputCommonOpts = {file: './dist/watch-it.common.js', format: 'cjs', exports: 'named'}
+const outputEsmOpts = {file: './dist/watch-it.es.js', format: 'esm', exports: 'named'}
 
-rollup.rollup(inputOpts)
-  .then(bundle => {
-    return bundle.generate(outputOpts).then(() => bundle.write(outputOpts))
-  })
-  .catch(e => console.log(e))
+build(outputIifeOpts)
+build(outputCommonOpts)
+build(outputEsmOpts)
+
+function build(outputOpts) {
+  rollup.rollup(inputOpts)
+    .then(bundle => {
+      return bundle.generate(outputOpts).then(() => bundle.write(outputOpts))
+    })
+    .catch(e => console.log(e))
+}
